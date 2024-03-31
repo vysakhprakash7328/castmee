0612cb88700680ef3fc8f3bbbabc7b92fdbc9015
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-03-31 18:19:14 +0530

    bugfix

--- castmeeproject/urls.py
+++ castmeeproject/urls.py
@@ -1,29 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Castme API",
-        default_version='v1',
-        description="Castme API",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,)
-)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('castmeeapp.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
