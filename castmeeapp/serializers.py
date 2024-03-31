308790143aa1d0b6a4cc49ea1d2b6c34411e6865
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-03-31 16:41:33 +0530

    major change is base models and api

--- castmeeapp/serializers.py
+++ castmeeapp/serializers.py
@@ -2,56 +2,34 @@ from rest_framework import serializers
 from .models import *
 
 
-class Talent_user_registration_serializer(serializers.ModelSerializer):
+class ArtistExtendedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Talent_user_details
-        fields = '__all__'
+        model = ArtistExtended
+        exclude = [
+            'languages_known','skills','consider_me_for','interest',
+            'available_for','preferred_format','preferred_genre',
+        ]
 
 
-class Get_talent_user_details_serializer(serializers.ModelSerializer):
+class ArtistExtendedSerializerView(serializers.ModelSerializer):
     '''
-    Talent user serializer with all data
+    Customized serializer for artist view
     '''
-    name = serializers.ReadOnlyField(source = 'user_id.first_name')
-    user_id= serializers.ReadOnlyField(source = 'user_id.id')
-    username = serializers.ReadOnlyField(source='user_id.username')
-    email = serializers.ReadOnlyField(source='user_id.email')
-
-    
     class Meta:
-        model = Talent_user_details
+        model = ArtistExtended
         fields = '__all__'
 
-class Talent_recruiter_registration_seriailizer(serializers.ModelSerializer):
+
+class ProducerExtendedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProducerExtended
+        fields = '__all__'
+
+
+class ProducerExtendedSerializerView(serializers.ModelSerializer):
     '''
-    Talent recruiter details with all data
+    Customized serializer for producer view
     '''
-    name =  serializers.ReadOnlyField(source = 'recruiter_id.first_name')
-    user_id = serializers.ReadOnlyField(source = 'recruiter_id.id')
-    username = serializers.ReadOnlyField(source = 'recruiter_id.username')
     class Meta:
-        model = Talent_recruiter_details
+        model = ProducerExtended
         fields = '__all__'
-
-class Talent_recruiter_freelancer_details_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = freelance_recruiter_details
-        fields = '__all__'
-
-class Talent_recruiter_company_details_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = company_recruiter_details
-        fields = '__all__'
-
-class Talent_user_language_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Talent_user_languages
-        fields = '__all__'
-class Talent_user_skills_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Talent_user_skills
-        fields = '__all__'
-
-
-
-
--- castmeeapp/urls.py
+++ castmeeapp/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from . import views
+from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('talent_user_registration_api/',views.Talent_user_registration_API.as_view()),
-    path('login_api/',views.Login_API.as_view()),
-    path('get_talent_user_details_api/',views.Get_talent_user_details_API.as_view()),
-    path('talent_recruiter_registration/',views.RegistrationAPI.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('get_dropdowns/', views.get_dropdowns, name='get_dropdowns'),
-    path('update_data/', views.UpdationAPI.as_view(), name='UpdationAPI'),
-    path('get_filtered_result/', views.FilterAPI.as_view(), name='filterresult'),
-    path('get_talent_user_data/', views.FilterAPI.as_view(), name='get_talent_data'),
+    path('artist_registration/', ArtistCreateAPIView.as_view(), name='artist_registration'),
+    path('producer_registration/', ProducerCreateAPIView.as_view(), name='producer_registration'),
+    path('login_artist/', ArtistLoginAPIView.as_view(), name='producer_registration'),
+    path('login_producer/', ProducerLogin.as_view(), name='producer_registration'),
+    path('dropdowns/', MasterDropdowns.as_view(), name='MasterDropdowns'),
+    path('update_artist/', ArtistExtendedUpdateAPIView.as_view(), name='ArtistExtendedUpdateAPIView'),
+    path('get_artist_user/', ArtistExtendedUpdateAPIView.as_view(), name='ArtistExtendedUpdateAPIView'),
+    path('update_producer/', ProducerExtendedAPIView.as_view(), name='ProducerExtendedAPIView'),
+    path('get_producer_user/', ProducerExtendedAPIView.as_view(), name='ProducerExtendedAPIView'),
+    path('get_artists/', FilterApi.as_view(), name='FilterApi'),
+    path('filter_artists/', FilterApi.as_view(), name='filter artists'),
 ]
