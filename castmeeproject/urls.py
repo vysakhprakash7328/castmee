4ea5ef6476088d18cece508c676858c37e27f035
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-04-09 22:56:41 +0530

    Admin module updation,project updation

--- castmeeproject/urls.py
+++ castmeeproject/urls.py
@@ -11,3 +11,4 @@ urlpatterns = [
     path('api/',include('castmeeapp.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
