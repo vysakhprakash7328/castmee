8ea89b45b9d9cce0412c512625580adeb7ccc585
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-04-10 11:37:32 +0530

    producer model and api change

--- castmeeproject/settings.py
+++ castmeeproject/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
