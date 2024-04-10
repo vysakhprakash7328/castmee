2fc126dda2725476a07937b402d7940fff797e75
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-04-10 11:47:20 +0530

    rebased dev

--- castmeeproject/settings.py
+++ castmeeproject/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-# from dotenv import load_dotenv
 
-# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
